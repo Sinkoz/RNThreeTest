1e0356b97759643978fe443a586cddd28b1be1b3
Xinchen <sinkozsinkoz@gmail.com>
2021-05-14 15:15:00 +0800

    feat: added navigation

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import { WebView } from 'react-native-webview'
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Dimensions} from "react-native-web";
 
-export default class App extends React.Component {
-  render() {
-    return <WebView source={{ uri: 'https://lifeinvogue.vogue.it/' }} />
-  }
+function HomeScreen() {
+  return (
+      <WebView style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height}} source={{ uri: 'https://lifeinvogue.vogue.it/' }} />
+  );
+}
+
+function WebViewOneScreen() {
+  return (
+      <WebView style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height}} source={{ uri: 'https://dark.netflix.io/en' }} />
+  );
+}
+
+function WebViewTwoScreen() {
+    return (
+          <WebView style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height}} source={{ uri: 'https://50-jahre-hitparade.ch/' }} />
+    );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen} />
+            <HomeStack.Screen name="Webview1" component={WebViewOneScreen} />
+        </HomeStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="WebView1" component={WebViewOneScreen} />
+        <Tab.Screen name="WebView2" component={WebViewTwoScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
 }
